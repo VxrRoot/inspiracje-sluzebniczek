9a06fcc82e53acdfb78f74e4720dab403f1e16a4
Dawid Słowik <dawid.slowik@visionect.io>
2023-06-08 12:42:10 +0200

    Update

--- src/app/losuj-cytat/page.tsx
+++ src/app/losuj-cytat/page.tsx
@@ -14,10 +14,11 @@ const DrawQuote: FC = () => {
   }, []);
 
   return (
-    <div className="flex justify-center items-center flex-col h-screen gap-12">
-      <p className="text-xl">{`"${Quotes[randomIndex].quote},,`}</p>
+    <div className="flex justify-center items-center flex-col h-screen gap-4 max-w-5xl px-6 mx-auto">
+      <p className="text-2xl">{`"${Quotes[randomIndex].quote},,`}</p>
+      <p className="text-l italic">{`${Quotes[randomIndex].author}`}</p>
       <button
-        className="px-4 py-2 border border-white hover:border-cyan-400 transition-colors"
+        className="px-4 py-2 border border-white hover:border-cyan-400 transition-colors mt-4"
         onClick={drawQuote}
       >
         Losuj kolejny
