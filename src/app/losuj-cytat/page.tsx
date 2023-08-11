e5476cce8a620b523d3abea9c523c66d421d6921
Dawid Słowik <dawid.slowik@visionect.io>
2023-08-11 11:49:01 +0200

    update bg

--- src/app/losuj-cytat/page.tsx
+++ src/app/losuj-cytat/page.tsx
@@ -14,16 +14,18 @@ const DrawQuote: FC = () => {
   }, []);
 
   return (
-    <div className="flex justify-center items-center flex-col h-screen gap-4 max-w-5xl px-6 mx-auto">
-      <p className="text-2xl text-center">{`"${Quotes[randomIndex].quote},,`}</p>
-      <p className="text-l italic">{`${Quotes[randomIndex].author}`}</p>
-      <button
-        className="px-4 py-2 border border-white hover:border-cyan-400 transition-colors mt-4"
-        onClick={drawQuote}
-      >
-        Losuj kolejny
-      </button>
-    </div>
+    <section className="bg-cover text-black  bg-center bg-mobile-bg md:bg-desktop-bg ">
+      <div className="flex justify-end pb-14 md:justify-center items-center flex-col h-screen gap-4 max-w-4xl px-6 mx-auto ">
+        <p className="text-2xl text-center">{`"${Quotes[randomIndex].quote},,`}</p>
+        <p className="text-l italic">{`${Quotes[randomIndex].author}`}</p>
+        <button
+          className="px-4 py-2 border border-black hover:border-cyan-400 transition-colors mt-4"
+          onClick={drawQuote}
+        >
+          Losuj kolejny
+        </button>
+      </div>
+    </section>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,21 @@ import HandsSvg from "../../public/hands.svg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-cover text-black  bg-center bg-desktop-bg ">
       <div className="tracking-wider flex flex-col gap-8 items-center">
-        <Image src={HandsSvg} alt="praying hands" width={150} height={150} />
+        <Image
+          src={HandsSvg}
+          alt="praying hands"
+          width={150}
+          height={150}
+          className="fill-black"
+        />
         <h1 className="text-center text-2xl uppercase">
           Inspiracje <br></br>
           służebniczek
         </h1>
         <Link href="/losuj-cytat" legacyBehavior>
-          <a className="px-4 py-2 border border-white hover:border-cyan-400 transition-colors">
+          <a className="px-4 py-2 border border-black hover:border-cyan-400 transition-colors">
             Losuj cytat
           </a>
         </Link>
